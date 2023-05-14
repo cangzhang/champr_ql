b745bcb2c5f0950d1aee5be73faf080f83581649
al <cangzan@gmail.com>
2023-05-14 15:53:32 +0800

    fix: remote source url

--- pages/api/sources.js
+++ pages/api/sources.js
@@ -1,13 +1,14 @@
 import fetch from 'node-fetch';
 
-const NPM_HOST = "https://registry.npmjs.com";
+const NPM_HOST = "https://registry.npmjs.com/@champ-r";
 
 export default async function handler(req, res) {
   let sources = [];
   try {
     const indexFile = await fetch(`${NPM_HOST}/source-list/latest`).then(r => r.json());
     sources = indexFile.sources;
-  } catch {
+  } catch (err) {
+    console.error(err);
     sources = [];
   }
 
